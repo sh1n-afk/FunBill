69b5b85bfcbb018a0ce86b13bc29f32fb6e22c84
Poornank Purohit <70952946+sh1n-afk@users.noreply.github.com>
2023-01-31 21:11:59 -0800

    Add files via upload

---
+++ Milestones/M2/VerticalPrototype/webapp/displayDB.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var database = require('../DB setup');
+
+router.get('/', (request, response, next) => {
+    var query = "SELECT * FROM Accounts";
+
+    database.query(query, (err, data) => {
+
+        if (err) console.log(err);
+
+        response.render('homepage', {title: 'Test Response...', action: 'list', filename: 'homepage.html'}, )
+    });
+});
+
+module.exports= router;
